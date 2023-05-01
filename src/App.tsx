8e691075fc778ff7521e72e88023b728878e542b
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-05-01 16:51:38 +0300

    feat: init project with React, With, Typescript, Eslint, Prettier, Stylelint, Husky, SCSS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import 'normalize.css'
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <>
+      <h1 className={styles.red_color}>Hello World!!!</h1>
+    </>
+  )
+}
+
+export default App
