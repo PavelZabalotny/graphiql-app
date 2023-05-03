b506fc77ed1aab5584bfb610d388fb16ee1e18ad
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-05-03 22:12:39 +0300

    feat: add Footer component

---
+++ src/common/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import { Container } from '@mui/material'
+
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Container>
+        <p>Footer</p>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
