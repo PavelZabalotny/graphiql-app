6e465556da68c6586671ec45238d8257c370a30e
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-05-03 22:13:08 +0300

    feat: add Hain component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
+import { CssBaseline } from '@mui/material'
 import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
@@ -6,6 +8,7 @@ import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
