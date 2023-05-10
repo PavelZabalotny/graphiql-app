1baa44cae6624e40a8bf7f45351b324406946506
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-05-11 00:43:05 +0300

    feat: implement redirecting to Home page if User logout

--- src/pages/GraphiQL/GraphiQL.tsx
+++ src/pages/GraphiQL/GraphiQL.tsx
@@ -1,6 +1,21 @@
 import { Container } from '@mui/material'
+import { useEffect } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+import { RoutePaths } from '@/routes/routerPaths.ts'
+import { useAppSelector } from '@/store/hooks.ts'
 
 const GraphiQl = () => {
+  const navigate = useNavigate()
+  const isUserLoggedIn = useAppSelector((state) => state.userReducer.isLoggedIn)
+
+  useEffect(() => {
+    if (!isUserLoggedIn) {
+      navigate(RoutePaths.Home)
+    }
+  }, [isUserLoggedIn, navigate])
+
   return <Container maxWidth={false}>GraphiQL</Container>
 }
 
