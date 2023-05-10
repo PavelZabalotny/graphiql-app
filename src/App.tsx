6c26eeacdfa6ac55351809223caedf11fb757a39
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-05-11 00:42:05 +0300

    feat: implement loading user logged in status when init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 
 import Loading from '@/common/Loading/Loading.tsx'
+import { auth } from '@/firebase/firebase.ts'
+import { setUserLoggedInStatus } from '@/reducers/userSlice.ts'
 import { router } from '@/routes/router.tsx'
+import { useAppDispatch, useAppSelector } from '@/store/hooks.ts'
 
 function App() {
-  return <RouterProvider router={router} fallbackElement={<Loading />} />
+  const isUserLoggedIn = useAppSelector((state) => state.userReducer.isLoggedIn)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(setUserLoggedInStatus(true))
+      } else {
+        dispatch(setUserLoggedInStatus(false))
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [dispatch])
+
+  return <>{isUserLoggedIn === null ? <Loading /> : <RouterProvider router={router} fallbackElement={<Loading />} />}</>
 }
 
 export default App
