8e8e2485d1ca4461be18ea4da363020cd8b7e7dd
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-05-11 00:45:53 +0300

    feat: implement changing buttons in dependence of User logged in status

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -4,9 +4,36 @@ import styles from './Header.module.scss'
 
 import CustomNavLink from '@/common/components/CustomNavLink/CustomNavLink.tsx'
 import { logout } from '@/firebase/firebase.ts'
+import { setUserLoggedInStatus } from '@/reducers/userSlice.ts'
 import { RoutePaths } from '@/routes/routerPaths'
+import { useAppDispatch, useAppSelector } from '@/store/hooks.ts'
 
 const Header = () => {
+  const isUserLoggedIn = useAppSelector((state) => state.userReducer.isLoggedIn)
+  const dispatch = useAppDispatch()
+
+  const logoutButtonsGroup = (
+    <>
+      <CustomNavLink to={RoutePaths.GraphiQL}>GraphiQL</CustomNavLink>
+      <Button
+        variant='contained'
+        onClick={() => {
+          logout()
+          dispatch(setUserLoggedInStatus(false))
+        }}
+      >
+        Logout
+      </Button>
+    </>
+  )
+
+  const loginButtonsGroup = (
+    <>
+      <CustomNavLink to={RoutePaths.SignIn}>Sign in</CustomNavLink>
+      <CustomNavLink to={RoutePaths.SignUp}>Sign up</CustomNavLink>
+    </>
+  )
+
   return (
     <header className={styles.header}>
       <Container>
@@ -14,16 +41,7 @@ const Header = () => {
           <Toolbar>
             <Stack spacing={5} direction='row'>
               <CustomNavLink to={RoutePaths.Home}>Home</CustomNavLink>
-              <CustomNavLink to={RoutePaths.SignIn}>Sign in</CustomNavLink>
-              <CustomNavLink to={RoutePaths.SignUp}>Sign up</CustomNavLink>
-              <Button
-                variant='contained'
-                onClick={() => {
-                  logout()
-                }}
-              >
-                Logout
-              </Button>
+              {isUserLoggedIn ? logoutButtonsGroup : loginButtonsGroup}
             </Stack>
           </Toolbar>
         </AppBar>
