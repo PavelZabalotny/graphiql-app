7d25793fdfd1e20960eedfef986722bf56d80f0f
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-05-12 00:13:41 +0300

    refactor: change Header

--- src/common/components/CustomNavLink/CustomNavLink.tsx
+++ src/common/components/CustomNavLink/CustomNavLink.tsx
@@ -1,12 +1,28 @@
-import { Button } from '@mui/material'
+import {
+  Button,
+  type ButtonPropsColorOverrides,
+  type ButtonPropsVariantOverrides,
+  type SxProps,
+  type Theme,
+} from '@mui/material'
+import { type OverridableStringUnion } from '@mui/types'
 import { type FC, type PropsWithChildren } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 interface Props {
   to: string
+  variant?: OverridableStringUnion<'text' | 'contained' | 'outlined', ButtonPropsVariantOverrides> | undefined
+  color?:
+    | OverridableStringUnion<
+        'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning',
+        ButtonPropsColorOverrides
+      >
+    | undefined
+  className?: string
+  sx?: SxProps<Theme> | undefined
 }
 
-const CustomNavLink: FC<PropsWithChildren<Props>> = ({ to, children }) => {
+const CustomNavLink: FC<PropsWithChildren<Props>> = ({ to, variant, color, className, sx, children }) => {
   const navigate = useNavigate()
 
   function handleClick() {
@@ -14,7 +30,7 @@ const CustomNavLink: FC<PropsWithChildren<Props>> = ({ to, children }) => {
   }
 
   return (
-    <Button color='primary' variant='contained' onClick={handleClick}>
+    <Button color={color} variant={variant} onClick={handleClick} className={className} sx={sx} size='large'>
       {children}
     </Button>
   )
--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,8 +1,9 @@
-import { AppBar, Button, Container, Stack, Toolbar } from '@mui/material'
+import { AppBar, Button, Container, type SxProps, type Theme } from '@mui/material'
 
 import styles from './Header.module.scss'
 
 import CustomNavLink from '@/common/components/CustomNavLink/CustomNavLink.tsx'
+import Logo from '@/common/components/Logo/Logo.tsx'
 import { logout } from '@/firebase/firebase.ts'
 import { setUserLoggedInStatus } from '@/reducers/userSlice.ts'
 import { RoutePaths } from '@/routes/routerPaths'
@@ -11,12 +12,18 @@ import { useAppDispatch, useAppSelector } from '@/store/hooks.ts'
 const Header = () => {
   const isUserLoggedIn = useAppSelector((state) => state.userReducer.isLoggedIn)
   const dispatch = useAppDispatch()
+  const buttonCustomStyles: SxProps<Theme> = {
+    backgroundColor: '#FE8205',
+    color: '#ffffff',
+    '&:hover': { backgroundColor: '#ff9434' },
+  }
 
   const logoutButtonsGroup = (
     <>
       <CustomNavLink to={RoutePaths.GraphiQL}>GraphiQL</CustomNavLink>
       <Button
         variant='contained'
+        sx={buttonCustomStyles}
         onClick={() => {
           logout()
           dispatch(setUserLoggedInStatus(false))
@@ -30,23 +37,22 @@ const Header = () => {
   const loginButtonsGroup = (
     <>
       <CustomNavLink to={RoutePaths.SignIn}>Sign in</CustomNavLink>
-      <CustomNavLink to={RoutePaths.SignUp}>Sign up</CustomNavLink>
+      <CustomNavLink to={RoutePaths.SignUp} variant='contained' color='inherit' sx={buttonCustomStyles}>
+        Sign up
+      </CustomNavLink>
     </>
   )
 
   return (
-    <header className={styles.header}>
+    <AppBar className={styles.header} color='inherit' position='sticky'>
       <Container>
-        <AppBar position='static' color='secondary'>
-          <Toolbar>
-            <Stack spacing={5} direction='row'>
-              <CustomNavLink to={RoutePaths.Home}>Home</CustomNavLink>
-              {isUserLoggedIn ? logoutButtonsGroup : loginButtonsGroup}
-            </Stack>
-          </Toolbar>
-        </AppBar>
+        <div className={styles.container}>
+          <Logo />
+          <CustomNavLink to={RoutePaths.Home}>Home</CustomNavLink>
+          <div className={styles.button_group}>{isUserLoggedIn ? logoutButtonsGroup : loginButtonsGroup}</div>
+        </div>
       </Container>
-    </header>
+    </AppBar>
   )
 }
 
