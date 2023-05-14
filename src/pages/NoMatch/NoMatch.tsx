1a9420cc5621372986f2640eff0575112b27d027
Evgeniia <Kochetkovaev11@gmail.com>
2023-05-14 09:07:00 +0300

    fix: fix styles

--- src/pages/NoMatch/NoMatch.tsx
+++ src/pages/NoMatch/NoMatch.tsx
@@ -1,9 +1,24 @@
-import { Container } from '@mui/material'
+import { Button, Container, type SxProps, type Theme } from '@mui/material'
+
+import styles from './NoMatch.module.scss'
 
 const NoMatch = () => {
+  const buttonCustomStyles: SxProps<Theme> = {
+    backgroundColor: '#FE8205',
+    color: '#FFFFFF',
+    '&:hover': { backgroundColor: '#ff9434' },
+  }
+
   return (
-    <Container>
-      <h2>Error 404!</h2>
+    <Container className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.text}>
+        Oh no! It looks like you&apos;ve stumbled upon the infamous Page 404 - the land of lost pages and digital
+        tumbleweeds.
+      </p>
+      <Button href='/' variant='contained' sx={buttonCustomStyles}>
+        Go to Home
+      </Button>
     </Container>
   )
 }
