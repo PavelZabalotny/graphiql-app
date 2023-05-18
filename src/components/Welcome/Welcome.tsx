a85acb8766eb17f06b53d4619daa0cc185889ada
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-05-18 19:41:46 +0200

    feat: add cards

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,43 +1,85 @@
 import { Container, Button } from '@mui/material'
 
+import { cards, type Card } from './data/cards'
+
 import styles from './Welcome.module.scss'
 
 const Welcome = () => {
   return (
     <section className={styles.welcome}>
-      <Container sx={{ height: 'fit-content', display: 'flex', flexDirection: 'column', backgroundColor: '#0198A5' }}>
+      <Container
+        sx={{
+          height: 'fit-content',
+          minWidth: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+        }}
+      >
         <div className='graph-team'>
           <div className='graphql'>
-            <h1>GraphiQL - Your Online Assistant for Learning and Testing GraphQL APIs</h1>
-            <p>
-              Explore, build, and test your GraphQL queries in a user-friendly environment. Enhance your API development
-              experience with our powerful and interactive GraphiQL tool.
-            </p>
-            <Button />
+            <div className='text-button'>
+              <h1>GraphiQL - Your Online Assistant for Learning and Testing GraphQL APIs</h1>
+              <p>
+                Explore, build, and test your GraphQL queries in a user-friendly environment. Enhance your API
+                development experience with our powerful and interactive GraphiQL tool.
+              </p>
+              <Button />
+            </div>
+            <div className='monitor-image'>
+              <img src='/Monitor.png' />
+              <img src='/screen.png' />
+            </div>
           </div>
           <div className='team'>
             <h2>Our team</h2>
-            <div className='cards'>Dynamically x3</div>
+            <div className='cards'>
+              {cards.map((card: Card) => (
+                <div key={card.id} className='card' onClick={() => window.open(`${card.github}`, '_blank')}>
+                  <div
+                    style={{
+                      width: '50px',
+                      height: '50px',
+                    }}
+                  >
+                    <img
+                      src={`${card.photo}`}
+                      className='photo'
+                      style={{
+                        backgroundPosition: 'center',
+                        backgroundSize: 'contain',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: '#fff',
+                      }}
+                      alt='photo'
+                    />
+                  </div>
+                  <h2>{card.name}</h2>
+                  <span>{card.location}</span>
+                  <p>{card.technologies}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <div className='project-course'>
           <div className='project'>
             <h2>About the project</h2>
             <div className='player'>
-              <div className='background-block'></div>
-              <div className='front-block'></div>
+              <div className='background-block' />
+              <div className='front-block' />
             </div>
           </div>
           <div className='course'>
             <h2>About the Course</h2>
             <div className='information'>
-              <div className='technologies'></div>
+              <div className='technologies' />
               <div className='description'>
                 <h3>React Course &#x0028;The Rolling Scopes&#x0029; </h3>
                 <div className='text'>
-                  <p></p>
-                  <p></p>
-                  <p></p>
+                  <p />
+                  <p />
+                  <p />
                 </div>
               </div>
             </div>
--- src/components/Welcome/data/cards.ts
+++ src/components/Welcome/data/cards.ts
@@ -4,22 +4,33 @@ export interface Card {
   name: string
   location: string
   technologies: string
+  github: string
 }
 
 export const cards: Card[] = [
   {
     id: 0,
-    photo: '',
-    name: '',
-    location: '',
-    technologies: '',
+    photo: 'https://avatars.githubusercontent.com/u/100534408?v=4',
+    name: 'Evgeniia Mokhova',
+    location: 'Moscow, Russia',
+    technologies:
+      'JavaScript, TypeScript, GIT, HTML5, CSS3, SASS, React.js, Vite, Next.js, Node.js, Express.js, MongoDB, Figma',
+    github: 'https://github.com/emoxowa',
   },
   {
     id: 1,
-    photo: '',
-    name: '',
-    location: '',
-    technologies: '',
+    photo: 'https://avatars.githubusercontent.com/u/41519166?v=4',
+    name: 'Pavel Zabalotny',
+    location: 'Minsk, Belarus',
+    technologies: 'Angular, React.js, JavaScript, TypeScript, Node.js, HTML5, CSS3, Figma, Vite, Webpack, Webstorm',
+    github: 'https://github.com/PavelZabalotny',
   },
-  { id: 2, photo: '', name: '', location: '', technologies: '' },
+  {
+    id: 2,
+    photo: 'https://avatars.githubusercontent.com/u/93344846?v=4',
+    name: 'Viyaleta Haponava',
+    location: 'Gdansk, Poland',
+    technologies: 'React.js, TypeScript, JavaScript, HTML5, CSS3, SASS, GIT, Webpack, Figma',
+    github: 'https://github.com/ViyaletaH',
+  },
 ]
