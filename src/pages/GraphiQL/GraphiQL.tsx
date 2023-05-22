8f19e3a797306d0ab7259d91183faed3043be863
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-05-22 23:48:50 +0300

    feat: implement documentation section

--- src/pages/GraphiQL/GraphiQL.tsx
+++ src/pages/GraphiQL/GraphiQL.tsx
@@ -1,11 +1,13 @@
-import { KeyboardArrowDown, PlayArrow } from '@mui/icons-material'
+import { AutoStories, Close, KeyboardArrowDown, PlayArrow, Undo } from '@mui/icons-material'
 import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
   Box,
   Container,
+  Drawer,
   Fab,
+  Link,
   Stack,
   TextField,
   Tooltip,
@@ -14,12 +16,15 @@ import {
   useTheme,
 } from '@mui/material'
 
+import { getIntrospectionQuery, type IntrospectionType } from 'graphql'
+import { type Maybe } from 'graphql/jsutils/Maybe'
 import { type ChangeEvent, useEffect, useState, type KeyboardEvent } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
 import { BREAKPOINT_MD } from '@/common/constants'
 import Loading from '@/common/Loading/Loading.tsx'
+import { httpIntrospectionQuery, type IntrospectionQueryResponse } from '@/common/services/httpIntrospectionQuery'
 import { httpSendRequest } from '@/common/services/httpSendRequest.ts'
 import { RoutePaths } from '@/routes/routerPaths.ts'
 import { useAppSelector } from '@/store/hooks.ts'
@@ -34,12 +39,17 @@ const defaultQuery = `{
 }
 `
 
+let schemaTypes: Maybe<IntrospectionType[]>
+
 const GraphiQl = () => {
   const navigate = useNavigate()
   const isUserLoggedIn = useAppSelector((state) => state.userReducer.isLoggedIn)
   const [query, setQuery] = useState(defaultQuery)
   const [variables, setVariables] = useState('')
   const [response, setResponse] = useState('')
+  const [schema, setSchema] = useState<JSX.Element[]>([])
+  const [showDrawer, setShowDrawer] = useState(false)
+  const [isSchemaLoading, setIsSchemaLoading] = useState(false)
   const [loading, setLoading] = useState(false)
   const theme = useTheme()
   const matches = useMediaQuery(theme.breakpoints.down(BREAKPOINT_MD))
@@ -74,6 +84,172 @@ const GraphiQl = () => {
       void sendQuery()
     }
   }
+
+  function generateSchema(name: string) {
+    let jsx: JSX.Element
+    const item = schemaTypes?.filter((value) => value.name === name)[0]
+
+    if (item) {
+      if (item.kind === 'OBJECT' || item.kind === 'INTERFACE') {
+        jsx = (
+          <>
+            <Typography variant='h6'>{item.name}</Typography>
+            <Typography variant='subtitle1'>{item.description}</Typography>
+            <ul>
+              {item.fields.map((fieldItem) => {
+                const { kind } = fieldItem.type
+
+                return (
+                  <li key={Math.random()}>
+                    <Typography variant='subtitle2'>
+                      {fieldItem.name}:
+                      {kind === 'NON_NULL' || kind === 'LIST' ? (
+                        fieldItem.type.ofType.kind === 'OBJECT' || fieldItem.type.ofType.kind === 'SCALAR' ? (
+                          <>
+                            {' '}
+                            <Link
+                              href='#'
+                              onClick={(e) => {
+                                generateSchema((e.target as HTMLLinkElement).innerText.trim())
+                              }}
+                            >
+                              {fieldItem.type.ofType.name}
+                            </Link>
+                          </>
+                        ) : null
+                      ) : (
+                        <>
+                          {' '}
+                          <Link
+                            href='#'
+                            onClick={(e) => {
+                              generateSchema((e.target as HTMLLinkElement).innerText.trim())
+                            }}
+                          >
+                            {fieldItem.type.name}
+                          </Link>
+                        </>
+                      )}
+                    </Typography>
+                    <Typography variant='body2'>{fieldItem.description}</Typography>
+                  </li>
+                )
+              })}
+            </ul>
+          </>
+        )
+      } else {
+        jsx = (
+          <>
+            <Typography variant='subtitle2'>{item.name}</Typography>
+            <Typography variant='body2'>{item.description}</Typography>
+          </>
+        )
+      }
+    }
+
+    setSchema((state) => [jsx, ...state])
+  }
+
+  async function handleGetDocumentation() {
+    setIsSchemaLoading(true)
+    const json: IntrospectionQueryResponse | undefined = await httpIntrospectionQuery(getIntrospectionQuery())
+    const types: IntrospectionType[] | undefined = json?.data.__schema.types.filter((item) => item.name[0] !== '_')
+
+    if (types) {
+      schemaTypes = types
+    }
+
+    const jsonList: JSX.Element = (
+      <>
+        <Typography variant='h6'>Docs</Typography>
+        <Typography>A GraphQL schema provides a root type for each kind of operation.</Typography>
+        <Typography variant='subtitle2'>All Schema Types</Typography>
+        <ul style={{ listStyle: 'none', padding: '0 10px' }}>
+          {types?.map((item: IntrospectionType) => {
+            const element = (
+              <li key={Math.random()}>
+                <Link
+                  href='#'
+                  onClick={() => {
+                    generateSchema(item.name)
+                  }}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            )
+
+            return element
+          })}
+        </ul>
+      </>
+    )
+
+    setSchema((state) => [jsonList, ...state])
+    toggleDrawer()
+  }
+
+  const toggleDrawer = () => {
+    if (showDrawer) {
+      setIsSchemaLoading(false)
+    }
+
+    setShowDrawer(!showDrawer)
+  }
+
+  const drawer = (
+    <Drawer
+      anchor='left'
+      open={showDrawer}
+      onClose={toggleDrawer}
+      sx={{
+        '& .MuiPaper-root': {
+          padding: '10px',
+          display: 'flex',
+          width: '600px',
+          [theme.breakpoints.down(BREAKPOINT_MD)]: {
+            width: '300px',
+          },
+        },
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginBottom: 1,
+        }}
+      >
+        {schema.length > 1 ? (
+          <Link
+            href='#'
+            onClick={() => {
+              setSchema((state) => state.slice(1))
+            }}
+          >
+            <Fab color='primary' size='large'>
+              <Undo fontSize='medium' color='inherit' />
+            </Fab>
+          </Link>
+        ) : null}
+
+        <Fab
+          color='primary'
+          size='large'
+          onClick={() => {
+            toggleDrawer()
+          }}
+          sx={{
+            marginLeft: 'auto',
+          }}
+        >
+          <Close fontSize='medium' color='inherit' />
+        </Fab>
+      </Box>
+      <Box sx={{ fontSize: '0.875rem' }}>{schema[0]}</Box>
+    </Drawer>
+  )
 
   return (
     <Box
@@ -115,17 +291,31 @@ const GraphiQl = () => {
           >
             <Box display='flex' alignItems='center' justifyContent='space-between'>
               <Typography fontWeight='bold'>Request section</Typography>
-              <Tooltip title='Execute query (Ctrl-Enter)' placement='top'>
-                <Fab
-                  color='warning'
-                  size='small'
-                  onClick={() => {
-                    handleSendRequest()
-                  }}
-                >
-                  <PlayArrow fontSize='large' color='inherit' />
-                </Fab>
-              </Tooltip>
+              <Stack flexDirection='row' gap={2}>
+                <Tooltip title='Show Documtation Explorer' placement='top'>
+                  <Fab
+                    color='primary'
+                    size='large'
+                    onClick={() => {
+                      void handleGetDocumentation()
+                    }}
+                    disabled={isSchemaLoading}
+                  >
+                    <AutoStories fontSize='medium' color='inherit' />
+                  </Fab>
+                </Tooltip>
+                <Tooltip title='Execute query (Ctrl-Enter)' placement='top'>
+                  <Fab
+                    color='warning'
+                    size='large'
+                    onClick={() => {
+                      handleSendRequest()
+                    }}
+                  >
+                    <PlayArrow fontSize='large' color='inherit' />
+                  </Fab>
+                </Tooltip>
+              </Stack>
             </Box>
             <TextField
               fullWidth={true}
@@ -215,6 +405,7 @@ const GraphiQl = () => {
           </Box>
         </Stack>
       </Container>
+      {drawer}
     </Box>
   )
 }
